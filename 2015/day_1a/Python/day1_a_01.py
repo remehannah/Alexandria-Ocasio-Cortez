d80aa5217365674bb212f0b2165ccfd31d4d7531
Jaleel-VS <jeremikress@gmail.com>
2021-12-30 22:25:49 +0200

    finish part one of day 1 - python

--- 2015/day_1a/Python/day1_a_01.py
+++ 2015/day_1a/Python/day1_a_01.py
@@ -2,9 +2,25 @@ from os import path
 
 dir_path = path.dirname(path.dirname(path.realpath(__file__)))
 
+class NotQuiteLisp:
+    def __init__(self) -> None:
+        self.instructions = {')': -1, '(': 1}
+        self.current_floor = 0
+
+    def get_current_floor(self, file_input):
+        with open(file_input, 'r') as file:
+            for line in file:
+                for char in line:
+                    self.current_floor += self.instructions[char]
+
+        print(self.current_floor)
+            
 
 
-with open(f"{dir_path}/input.txt", 'r') as f:
-    for line in f:
-        for char in line:
+
+if __name__ == "__main__":
+    my_solution = NotQuiteLisp()
+    my_solution.get_current_floor(f"{dir_path}/input.txt")
+
+
 
