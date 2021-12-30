01a555d89166f1fd4bb4abfedbf4250f1f44bd3d
Jaleel-VS <jeremikress@gmail.com>
2021-12-30 23:49:48 +0200

    add C# solution

---
+++ 2015/day_1/C#/NotQuiteLisp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NotQuiteLisp
+{
+    class NotQuiteLisp
+    {
+        private readonly Dictionary<char, int> _instructions = new Dictionary<char, int>
+        {
+            {')', -1},
+            {'(', 1}
+        };
+
+        private int _currentFloor = 0;
+        private int _characterPosition = 0;
+
+        void TraverseThroughFloors(string path)
+        {
+            bool basementFound = false;
+
+            var lines = File.ReadLines(path);
+
+            foreach (var line in lines)
+            {
+                foreach (var symbol in line.ToCharArray())
+                {
+                    _currentFloor += _instructions[symbol];
+
+                    if (basementFound != false) continue;
+                    _characterPosition += 1;
+                    if (_currentFloor != -1) continue;
+                    Console.WriteLine($"The basement is located at position: {_characterPosition}");
+                    basementFound = true;
+                }
+            }
+
+            Console.WriteLine($"Santa is on floor: {_currentFloor}");
+        }
+
+
+        static void Main()
+        {
+            NotQuiteLisp notQuiteLisp = new NotQuiteLisp();
+            const string filePath = "/home/jaleelvs/Desktop/AOC/2015/day_1/input.txt";
+            notQuiteLisp.TraverseThroughFloors(filePath);
+        }
+    }
+}
