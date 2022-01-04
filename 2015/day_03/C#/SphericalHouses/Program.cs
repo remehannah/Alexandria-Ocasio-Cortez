f149d9de1f9abbd63f45624c394009046fd15323
Jaleel-VS <jeremikress@gmail.com>
2022-01-04 16:36:31 +0200

    solve part 1 and 2 in c#

--- 2015/day_03/C#/SphericalHouses/Program.cs
+++ 2015/day_03/C#/SphericalHouses/Program.cs
@@ -1,7 +1,74 @@
 class SphericalHouses
 {
+    private static Dictionary<char, int> santaCoordinates = new Dictionary<char, int>
+    {
+        {'x', 0},
+        {'y', 0},
+    };
+
+    private static Dictionary<char, int> roboSantaCoordinates = new Dictionary<char, int>
+    {
+        {'x', 0},
+        {'y', 0},
+    };
+
+    static HashSet<string> coordinates = new HashSet<string> {$"0,0"};
+    static string directions = File.ReadAllText("/home/jaleelvs/Desktop/AOC/2015/day_03/input.txt");
+
+    static string GetCoordinates(char symbol, Dictionary<char, int> coordinate)
+    {
+        switch (symbol)
+        {
+            case '^':
+                ++coordinate['y'];
+                break;
+            case '>':
+                ++coordinate['x'];
+                break;
+            case '<':
+                --coordinate['x'];
+                break;
+            case 'v':
+                --coordinate['y'];
+                break;
+        }
+
+        return $"{coordinate['x']},{coordinate['y']}";
+    }
+
+    static void SantaSolo()
+    {
+        foreach (var direction in directions)
+        {
+            coordinates.Add(GetCoordinates(direction, santaCoordinates));
+        }
+
+        Console.WriteLine(coordinates.Count);
+    }
+
+    static void SantaAndRobo()
+    {
+        foreach (var direction in directions.Select((value, index) => new {value, index}))
+        {
+            if (direction.index % 2 == 0)
+            {
+                coordinates.Add(GetCoordinates(direction.value, roboSantaCoordinates));
+            }
+
+            else
+            {
+                coordinates.Add(GetCoordinates(direction.value, santaCoordinates));
+            }
+            
+        }
+        Console.WriteLine(coordinates.Count);
+
+
+    }
+    
     static void Main()
     {
-        Console.WriteLine("It's a new day");
+       SantaAndRobo();
     }
 }
+
