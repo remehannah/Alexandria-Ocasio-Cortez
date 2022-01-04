64ea6b29d02f28bc390463329a70df7ac7c35f63
Jaleel-VS <jeremikress@gmail.com>
2022-01-04 09:27:34 +0200

    finish day_2 part b

--- 2015/day_2/C#/NoMath/Program.cs
+++ 2015/day_2/C#/NoMath/Program.cs
@@ -2,10 +2,16 @@
 {
     static string[] dimensions = File.ReadAllLines("/home/jaleelvs/Desktop/AOC/2015/day_2/input.txt");
 
+    // Part one
     static double CalculateSurfaceArea(int l, int w, int h)
     {
         int[] dimensionsArray = new int[] {(2 * l * w), (2 * w * h), (2 * h * l)};
         return dimensionsArray.Min() / 2 + dimensionsArray.Sum();
+    }
+
+    static double CalculateRibbonLength(int x, int y, int z)
+    {
+        return (2 * x + 2 * y) + (x * y * z);
     }
 
     static double TotalSurfaceArea1()
@@ -31,11 +37,37 @@
             .Select(x => 3 * x[0] * x[1] + 2 * x[0] * x[2] + 2 * x[1] * x[2])
             .Sum();
     }
+    // Part two
+    
+    static double TotalRibbonLength1()
+    {
+        double totalRibbonLength = 0;
+        foreach (string dimension in dimensions)
+        {
+            int[] dims = Array.ConvertAll(dimension.Split('x'), int.Parse);
+            Array.Sort(dims);
+            Tuple<int, int, int> points = Tuple.Create(dims[0], dims[1], dims[2]);
+            var (x, y, z) = points;
+            totalRibbonLength += CalculateRibbonLength(x, y, z);
+        }
 
+        return totalRibbonLength;
+    }
+    static double TotalRibbonLength2()
+    {
+        return dimensions
+            .Select(x => x.Split('x'))
+            .Select(x => x.Select(int.Parse))
+            .Select(x => x.OrderBy(y => y).ToArray())
+            .Select(x => 2 * x[0] + 2 * x[1] + x[0] * x[1] * x[2])
+            .Sum();
+    }
 
     static void Main()
     {
         Console.WriteLine($"Total Surface Area is: {TotalSurfaceArea1()}");
         Console.WriteLine($"Total Surface Area is: {TotalSurfaceArea2()}");
+        Console.WriteLine($"Total Ribbon Length is: {TotalRibbonLength1()}");
+        Console.WriteLine($"Total Ribbon Length is: {TotalRibbonLength2()}");
     }
 }
